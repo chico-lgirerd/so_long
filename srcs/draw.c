4690157da7266f59907f856eb5533140f232fb2c
Louis Girerd <lgirerd@z4r12p3.42lyon.fr>
2025-02-27 17:58:28 +0100

    add : all textures except player

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: lgirerd <lgirerd@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 11:26:29 by lgirerd           #+#    #+#             */
-/*   Updated: 2025/02/27 15:48:56 by lgirerd          ###   ########lyon.fr   */
+/*   Updated: 2025/02/27 17:38:44 by lgirerd          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,10 +40,31 @@ void	draw_colls(t_data *data, int x, int y)
 	int	width;
 	int	height;
 
-	data->img.img_ptr = mlx_xpm_to_image(data->mlx_ptr, "img/colls.xpm", &width, &height);
+	data->img.img_ptr = mlx_xpm_file_to_image(data->mlx_ptr, "img/jerry.xpm", &width, &height);
 	if (!data->img.img_ptr)
 		ft_mlx_error(data, "Failed to get image");
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img_ptr, x, y);	
+}
+
+void	draw_start_exit(t_data *data, int x, int y, char c)
+{
+	int	width;
+	int	height;
+
+	if (c == 'P')
+	{
+		data->img.img_ptr = mlx_xpm_file_to_image(data->mlx_ptr, "img/start.xpm", &width, &height);
+		if (!data->img.img_ptr)
+			ft_mlx_error(data, "Failed to get image");
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img_ptr, x, y);
+	}
+	else if (c == 'E')
+	{
+		data->img.img_ptr = mlx_xpm_file_to_image(data->mlx_ptr, "img/exit8.xpm", &width, &height);
+		if (!data->img.img_ptr)
+			ft_mlx_error(data, "Failed to get image");
+		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.img_ptr, x, y);	
+	}
 }
 
 void	draw_map(t_data *data)
@@ -63,6 +84,8 @@ void	draw_map(t_data *data)
 				draw_space(data, j * 64, i * 64);
 			else if (data->map[i][j] == 'C')
 				draw_colls(data, j * 64, i * 64);
+			else if (data->map[i][j] == 'P' || data->map[i][j] == 'E')
+				draw_start_exit(data, j * 64, i * 64, data->map[i][j]);
 			j++;
 		}
 		i++;
